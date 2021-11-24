ce888c25b8ee3fe3dfe52388d8036028c336ee5f
Dan Katorza <dkatorza@gmail.com>
2021-11-24 11:24:42 +0200

    1. connecting App.jsx to Store. 2. Changing naming from app-header to AppHeader.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,70 @@
 import React from "react";
-import { Switch, Route } from "react-router";
+import { Switch, Route,withRouter } from "react-router";
 import routes from "./routes";
+import { AppHeader } from './cmps/AppHeader';
 import { connect } from 'react-redux';
 import { userService } from "./services/user.service";
 
-export class RootCmp extends React.Component {
-  state = {
-    user: userService.getLoggedinUser()
+class _App extends React.Component {
+
+   componentDidMount() {
+    try {
+      const {loggedInUser} = this.props
+    }catch (err) {
+      console.log(err);
+    }
   }
-  componentDidMount = () => {
-    console.log("loggedInUser", userService.getLoggedinUser(), this.state.user);
+
+  async componentWillUnmount() {
+    const {loggedInUser} = this.props
+    if (loggedInUser) {
+      await userService.update(loggedInUser)
+    }
   }
 
+ 
+  get style() {
+    const {board,location} = this.props
+    if (!location.pathname.includes('/board')) return {}
+      const style = board ? {
+        background: `${board.style.background} center center / cover`,
+      }: {background: "#0079bf"} 
+      return style
+  }
 
-
-  render() {
-
-   
-
+  get isHeaderAppears(){
+    const {pathname} = this.props.location
+    return (pathname.includes('/board') || pathname.includes('workspace'))
+  }
+ 
+  
+  render(){
+    const {loggedInUser, board,location} = this.props
     return (
-      <div>
-     
-          <Switch>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                exact
-                component={route.component}
-                path={route.path}
-              />
-            ))}
-          </Switch>
-       
-        {/* <AppFooter /> */}
-      </div>
-    );
+      <section style={this.style}>
+        {this.isHeaderAppears && <header>
+          <AppHeader board={board} loggedInUser={loggedInUser} isBoardStyle={location.pathname.includes('/board')}/>
+          </header>}
+
+          <main>
+            <Switch>
+              {routes.map(route=><Route key={route.path} component={route.component} path={route.path}/>)}
+            </Switch>
+          </main>
+      </section>
+    )
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    board: state.boardModule.board,
+    loggedInUser: state.appModule.loggedInUser,
+  }
+}
+
+const mapDispatchToProps = {
+}
+
+const _AppWithRouter = withRouter(_App)
+export const App = connect(mapStateToProps, mapDispatchToProps)(_AppWithRouter)
