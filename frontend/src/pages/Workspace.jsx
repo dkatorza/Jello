a2ef472a422a898f591f9b1fd48f5d24a5f53da7
Dan Katorza <dkatorza@gmail.com>
2021-11-25 20:18:29 +0200

    1.Edited Workspace.jsx. 2. Added CardDetails.jsx(empty). 3. Added updated styles.scss. 4. Added workspace scss.

--- frontend/src/pages/Workspace.jsx
+++ frontend/src/pages/Workspace.jsx
@@ -1,84 +1,63 @@
 import React from "react";
 import { connect } from "react-redux";
-import { onSaveBoards,loadBoards } from '../store/board.actions';
-import { AppHeader } from "../cmps/app-header";
-import { BoardsList } from '../cmps/boards-list';
+import { onSaveBoard, loadBoards } from '../store/board.actions';
+import { BoardList } from '../cmps/BoardList'
 import { ReactComponent as BoardIcon } from "../assets/img/board-icon.svg";
-import { ReactComponent as StarIcon } from "../assets/img/star-icon.svg";
 
-class _BoardList extends React.Component {
- 
-  async componentDidMount() {
-    try {
-    const userId = await this.props.loggedUser._ID;
-    await this.props.loadBoards(userId);
-     } catch (err) {
-    console.log('err');
+class _Workspace extends React.Component {
+  componentDidMount() {
+    this.props.loadBoards()
   }
-}  
 
   get favoriteBoards() {
     const { boards } = this.props
     return boards.filter(board => board.isFavorite)
-  };  
-  get notFavoriteBoards() {
-    const { boards } = this.props
-    return boards.filter(board => !board.isFavorite)
-  };  
+  }
 
   onToggleFavorite = (ev, boardId) => {
     ev.preventDefault()
-    const { boards,onSaveBoards } = this.props
+    const { boards, onSaveBoard } = this.props
     const board = boards.find(board => board._id === boardId)
     board.isFavorite = !board.isFavorite
-    onSaveBoards(boards);
-  };
+    onSaveBoard(board)
+  }
 
   render() {
-    const { boards, loggedUser } = this.props;
-    if (!boards) return <div>Loading</div>;
+    const { boards } = this.props
+    if (!boards) return <Loader />
     return (
-      <section className="board-page">
-        <AppHeader />
-        <section className="board-list-container flex align-flex-start justify-center">
-          <div className="boards-wrapper flex column">
-            <div className="boards-preview flex column">
-              <div className="preview-title flex align-center">
-                <StarIcon className=" far fa-star"/>
-                <h3 className="flex"> Starred boards</h3>
-              </div>
-              <BoardsList onToggleFavorite={this.onToggleFavorite} boards={this.favoriteBoards} />
+      <section className="workspace-container flex align-flex-start justify-center ">
+        <div className="boards-wrapper flex column">
+          <div className="boards-preview flex column">
+            <div className="preview-title flex align-center">
+              <i className="far fa-star"></i>
+              <h3>Starred boards</h3>
             </div>
-
-            <div className={"boards-preview"}>
-              <div className={"preview-title flex align-center"}>
-                 <BoardIcon /> 
-                <h3 className="flex">
-                  {/* {loggedUser.username}'s Workspaces */}
-                  Workspaces
-                </h3>
-              </div>
-              <BoardsList onToggleFavorite={this.onToggleFavorite} boards={this.notFavoriteBoards} />
-            </div>
+            <BoardList onToggleFavorite={this.onToggleFavorite} boards={this.favoriteBoards} />
           </div>
-        </section>
+          <div className="boards-preview">
+            <div className="preview-title flex align-center">
+              <BoardIcon />
+              <h3>Workspace</h3>
+            </div>
+            <BoardList onToggleFavorite={this.onToggleFavorite} boards={boards} />
+          </div>
+        </div>
       </section>
-    );
+    )
   }
 }
 
 function mapStateToProps(state) {
   return {
-    loggedUser: state.userModule.user,
     boards: state.boardModule.boards,
-  };
+    loggedInUser: state.appModule.loggedInUser
+  }
 }
-const mapDispatchToProps = {
-  onSaveBoards,
-  loadBoards,
-};
 
-export const BoardList = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(_BoardList);
+const mapDispatchToProps = {
+  loadBoards,
+  onSaveBoard,
+}
+
+export const Workspace = connect(mapStateToProps, mapDispatchToProps)(_Workspace)
