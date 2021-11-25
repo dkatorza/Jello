9795249f719061a4ee65fa8e78b60a416eefde8c
Dan Katorza <dkatorza@gmail.com>
2021-11-25 22:05:01 +0200

    1.Edit HomeHeader. 2. Edit scss for HomeHeader. 3. Edit ScreenOverlay.

---
+++ frontend/src/cmps/HomeHeader.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { ReactComponent as HomepageLogo } from '../assets/img/logos/home-logo.svg'
+
+
+export class HomeHeader extends React.Component {
+  state = {
+    isNavBgVisible: false
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = () => {
+    const { isNavBgVisible } = this.state
+    if (window.pageYOffset > 100 && !isNavBgVisible) {
+      this.setState({ isNavBgVisible: true })
+    } else if (window.pageYOffset < 100 && isNavBgVisible) {
+      this.setState({ isNavBgVisible: false })
+    }
+  }
+
+
+  render() {
+    const { isNavBgVisible } = this.state
+    return (
+      <header className={`home-header ${isNavBgVisible ? 'visibleBg' : ''}`}>
+        <nav className="flex justify-space-between">
+          <div className="logo">
+            <HomepageLogo />
+            Jello
+          </div>
+
+          <div className="nav-btns">
+            <Link to="/login" className="login-btn clean-link">
+              Log in
+            </Link>
+            <Link to="/signup" className="signup-btn clean-link">
+              Sign up
+            </Link>
+          </div>
+        </nav>
+      </header>
+    )
+  }
+}
---
+++ frontend/src/cmps/ScreenOverlay.jsx
@@ -0,0 +1,12 @@
+export function ScreenOverlay({ goBack = null, styleMode, children }) {
+
+    return (
+        <div className="screen-overlay-wrapper">
+            <div className={`screen-overlay ${styleMode}`} onClick={() => {
+                if (goBack) goBack()
+                return
+            }}></div>
+            {children}
+        </div>
+    )
+}
